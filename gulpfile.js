a52a123b65781d11323445ab5400311040a9681d
thezeeck <estigma.e@gmail.com>
2016-11-03 14:36:25 -0600

    config

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ gulp.task( 'deploy', function () {
 
     // using base = '.' will transfer everything to /public_html correctly
     // turn off buffering in gulp.src for best performance
-
+ .src(config.jade.test, {base: 'dist/'})
     return gulp.src( globs, { base: '.', buffer: false } )
         .pipe( conn.newer( '/' ) ) // only upload newer files
         .pipe( conn.dest( '/' ) );
